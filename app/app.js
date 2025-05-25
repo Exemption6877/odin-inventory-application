479efe76f37e9b68ad2bb22da7695612a5097213
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-25 22:09:42 +0300

    Setup ejs viewer

--- app/app.js
+++ app/app.js
@@ -3,7 +3,15 @@ const app = express();
 
 require("dotenv").config({ path: "../.env" });
 
+const path = require("node:path");
 const PORT = process.env.APP_PORT;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
