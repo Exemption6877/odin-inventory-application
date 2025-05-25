41b15ab18f1c0924c47d5074733abc284b79d84b
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-26 02:41:26 +0300

    Add main controller/router. Change class in category.ejs

--- app/app.js
+++ app/app.js
@@ -5,15 +5,17 @@ const app = express();
 
 require("dotenv").config({ path: "../.env" });
 
+const inventoryRouter = require("./routers/inventoryRouter");
+
 const PORT = process.env.APP_PORT;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// Routes
+
+app.get("/", inventoryRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
---
+++ app/routers/inventoryRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+
+const inventoryRouter = Router();
+
+inventoryRouter.get("/", (req, res) => res.render("index"));
+
+module.exports = inventoryRouter;
