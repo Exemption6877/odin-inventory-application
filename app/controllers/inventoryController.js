7ded6ea52eeced8bdc90b50a99709fffde4ab739
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-27 05:50:38 +0300

    Output category navbar correctly

--- app/controllers/inventoryController.js
+++ app/controllers/inventoryController.js
@@ -3,7 +3,9 @@ const db = require("../db/queries");
 async function getAll(req, res) {
   try {
     const items = await db.getAllInventory();
-    res.render("index", { items: items });
+    const categories = await db.getAllCategories();
+
+    res.render("index", { items: items, categories: categories });
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
--- app/db/queries.js
+++ app/db/queries.js
@@ -13,4 +13,14 @@ GROUP BY i.price, i.discount, i.available, g.title, g.release_date, g.cover_url,
   return rows;
 }
 
-module.exports = { getAllInventory };
+async function getAllCategories() {
+  const { rows } = await pool.query(`SELECT DISTINCT name, COUNT(name) AS amount
+FROM categories AS c
+JOIN game_category AS gc ON c.id = gc.category_id
+GROUP by name
+ORDER BY amount Desc
+`);
+  return rows;
+}
+
+module.exports = { getAllInventory, getAllCategories };
