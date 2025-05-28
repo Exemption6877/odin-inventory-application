5d927cb60a8d6b245b7889392ed46b37c08cd1ac
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-28 17:38:29 +0300

    Use URL for DB connection

--- app/db/pool.js
+++ app/db/pool.js
@@ -3,9 +3,5 @@ const { Pool } = require("pg");
 require("dotenv").config({ path: "../.env" });
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: DB_CONNECTION_STRING,
 });
