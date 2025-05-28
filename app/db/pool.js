b2ff21d366de2446dbdf625b03c6c3e48f17c679
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-28 17:54:20 +0300

    Fix env

--- app/db/pool.js
+++ app/db/pool.js
@@ -3,5 +3,5 @@ const { Pool } = require("pg");
 require("dotenv").config({ path: "../.env" });
 
 module.exports = new Pool({
-  connectionString: DB_CONNECTION_STRING,
+  connectionString: process.env.DB_CONNECTION_STRING,
 });
